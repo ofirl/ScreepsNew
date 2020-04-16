931635d69d72bdce58812087ac61a8670963cc7c
Ofir Levi <ofirl24@gmail.com>
2020-04-16 22:16:01 +0300

    fix

--- Creep.js
+++ Creep.js
@@ -6,16 +6,21 @@
 
 Creep.prototype.collectEnergy = function () {
     // Collect energy.
-    let {target, type} = this.getTargetUnion({
+    let { target, type } = this.getTargetUnion({
         energy: {
             selector: () => this.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
                 filter: (resource) => resource.resourceType === RESOURCE_ENERGY && resource.amount >= this.store.getFreeCapacity[RESOURCE_ENERGY]
             }),
         },
+        // energy2: {
+        //     selector: () => _.max(this.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
+        //         filter: (resource) => resource.resourceType === RESOURCE_ENERGY
+        //     }), 'amount'),
+        // },
         energy2: {
-            selector: () => _.max(this.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
+            selector: () => this.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
                 filter: (resource) => resource.resourceType === RESOURCE_ENERGY
-            }), 'amount'),
+            }),
         },
         miner: {
             selector: () => _.max(this.room.find(FIND_MY_CREEPS, {
@@ -29,13 +34,13 @@ Creep.prototype.collectEnergy = function () {
             case 'energy':
             case 'energy2': {
                 if (this.pickup(target) === ERR_NOT_IN_RANGE) {
-                    this.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                    this.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
                 }
                 break;
             }
             case 'miner': {
                 if (target.transfer(this) === ERR_NOT_IN_RANGE) {
-                    this.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                    this.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
                 }
                 break;
             }
