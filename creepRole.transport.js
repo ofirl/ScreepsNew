cdfc8b050ce7191be7126f69558c97f46f7fae95
Ofir Levi <ofirl24@gmail.com>
2020-04-17 21:32:54 +0300

    test

--- creepRole.transport.js
+++ creepRole.transport.js
@@ -52,10 +52,10 @@ function run(creep) {
                         (s) => s.store.getCapacity(RESOURCE_ENERGY) - s.store[RESOURCE_ENERGY]),
                     // validator: (s) => s.store && s.store[RESOURCE_ENERGY] < s.store.getCapacity(RESOURCE_ENERGY),
                 },
-                tower: {
-                    selector: () => creep.room.find(FIND_MY_STRUCTURES, { 
-                        filter: (s) => s.structureType === STRUCTURE_TOWER && s.store && s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-                    }),
+                // tower: {
+                //     selector: () => creep.room.find(FIND_MY_STRUCTURES, { 
+                //         filter: (s) => s.structureType === STRUCTURE_TOWER && s.store && s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+                //     }),
                     // validator: (t) => t.store.getFreeCapacity(RESOURCE_ENERGY) > 0
                 },
                 upgrader: {
