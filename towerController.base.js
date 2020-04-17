a82978cb450de833676de254a3c25a2c293b2747
Ofir Levi <ofirl24@gmail.com>
2020-04-17 17:26:45 +0300

    fix tower

--- towerController.base.js
+++ towerController.base.js
@@ -2,9 +2,7 @@ function run(tower) {
     let hostiles = tower.room.find(FIND_HOSTILE_CREEPS);
     if (hostiles.length > 0) {
         let username = hostiles[0].owner.username;
-        Game.notify(`User ${username} spotted in room ${roomName}`);
-        // var towers = Game.rooms[roomName].find(
-        //     FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
+        Game.notify(`User ${username} spotted in room ${tower.room.name}`);
         tower.attack(hostiles[0]);
     }
 };
