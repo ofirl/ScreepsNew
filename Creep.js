65d8ea6b804ea14757b4dba273cb5122674cc882
Ofir Levi <ofirl24@gmail.com>
2020-04-17 12:06:04 +0300

    fix

--- Creep.js
+++ Creep.js
@@ -24,43 +24,43 @@ Creep.prototype.collectEnergy = function () {
         //     // validator: (creep) => creep.carry.energy > 0,
         // },
         container: {
-            selector: () => this.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+            selector: () => this.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: (structure) => structure.structureType === STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] >= this.store.getFreeCapacity[RESOURCE_ENERGY]
             }),
             // validator: (creep) => creep.carry.energy > 0,
         },
         container2: {
-            selector: () => this.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (i) => i.structureType == STRUCTURE_CONTAINER &&
-                    i.store[RESOURCE_ENERGY] > 0
-            }),
-    // validator: (creep) => creep.carry.energy > 0,
-    // selector: () => _.max(this.room.find(FIND_MY_STRUCTURES, {
-    //     filter: (structure) => structure.structureType === STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 0
-    // }), (structure) => structure.store[RESOURCE_ENERGY]),
-    // // validator: (creep) => creep.carry.energy > 0,
-},
+            //         selector: () => this.pos.findClosestByPath(FIND_STRUCTURES, {
+            //             filter: (i) => i.structureType == STRUCTURE_CONTAINER &&
+            //                 i.store[RESOURCE_ENERGY] > 0
+            //         }),
+            // // validator: (creep) => creep.carry.energy > 0,
+            selector: () => _.max(this.room.find(FIND_STRUCTURES, {
+                filter: (structure) => structure.structureType === STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 0
+            }), (structure) => structure.store[RESOURCE_ENERGY]),
+            // validator: (creep) => creep.carry.energy > 0,
+        },
     }, 'collectFrom');
 
-if (target) {
-    switch (type) {
-        case 'energy':
-        case 'energy2':
-            if (this.pickup(target) === ERR_NOT_IN_RANGE) {
-                this.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
-            }
-            break;
-        case 'miner':
-            if (target.transfer(this) === ERR_NOT_IN_RANGE) {
-                this.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
-            }
-            break;
-        case 'container':
-        case 'container2':
-            if (this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                this.moveTo(target);
-            }
-            break;
+    if (target) {
+        switch (type) {
+            case 'energy':
+            case 'energy2':
+                if (this.pickup(target) === ERR_NOT_IN_RANGE) {
+                    this.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
+                }
+                break;
+            case 'miner':
+                if (target.transfer(this) === ERR_NOT_IN_RANGE) {
+                    this.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
+                }
+                break;
+            case 'container':
+            case 'container2':
+                if (this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    this.moveTo(target);
+                }
+                break;
+        }
     }
-}
 };
