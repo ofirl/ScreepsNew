a91fe77b5bec68c47a91210c259fc42cacd082b4
Ofir Levi <ofirl24@gmail.com>
2020-04-17 11:25:15 +0300

    added repairs

--- creepRole.builder.js
+++ creepRole.builder.js
@@ -3,6 +3,7 @@
 const STATE_COLLECTING = 1;
 const STATE_MOVING = 2;
 const STATE_BUILDING = 3;
+const STATE_REPAIRING = 4;
 
 function parts(energy) {
     // TODO. Scale miner upto 5 work parts.
@@ -18,7 +19,7 @@ function run(creep) {
         case STATE_COLLECTING:
             if (creep.store[RESOURCE_ENERGY] === creep.store.getCapacity()) {
                 creep.say('⚡ deliver');
-                creep.memory.state = STATE_BUILDING;
+                creep.memory.state = STATE_REPAIRING;
                 // Clear the collection point as we wish to reselect collection target for next collection.
                 delete creep.memory.collectFrom;
                 run(creep);
@@ -65,6 +66,7 @@ function run(creep) {
             }
             else {
                 delete creep.memory.tid;
+
                 // Find next target
                 constructSite = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
                 if (constructSite) {
@@ -82,6 +84,45 @@ function run(creep) {
             }
 
             return;
+        case STATE_REPAIRING: {
+            if (creep.store[RESOURCE_ENERGY] === 0) {
+                creep.say('🔄 collect');
+                creep.memory.state = STATE_COLLECTING;
+                run(creep);
+                return;
+            }
+
+            let repairTarget;
+
+            if (creep.memory.rid)
+                repairTarget = Game.getObjectById(creep.memory.rid);
+            if (!repairTarget)
+                delete creep.memory.rid;
+
+            if (!repairTarget) {
+                const targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: object => object.hits / object.hitsMax < 0.7
+                });
+
+                targets.sort((a, b) => a.hits / a.hitsMax - b.hits / b.hitsMax);
+
+                if (targets.length > 0) {
+                    creep.memory.rid = targets[0].id;
+                    repairTarget = targets[0];
+                }
+            }
+
+            if (repairTarget) {
+                if (creep.repair(repairTarget) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(repairTarget);
+                }
+            }
+            else {
+                creep.memory.state = STATE_BUILDING;
+                run(creep);
+                return;
+            }
+        }
     }
 }
 
