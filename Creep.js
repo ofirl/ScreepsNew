6870586f95d65adf2473fc663c36349a902513c6
Ofir Levi <ofirl24@gmail.com>
2020-04-17 10:51:29 +0300

    fix

--- Creep.js
+++ Creep.js
@@ -42,7 +42,7 @@ Creep.prototype.collectEnergy = function () {
                 // validator: (creep) => creep.carry.energy > 0,
             },
         }, 'collectFrom');
-    if (targetU) {
+    if (targetU.target) {
         this.memory.collectFrom = { target: targetU.target.id, type: targetU.type };
         let { target, type } = targetU;
         switch (type) {
