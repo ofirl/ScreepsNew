7deffcd300c367c0fbf2105351851adaa8442145
Ofir Levi <ofirl24@gmail.com>
2020-04-21 16:03:14 +0300

    fix

--- towerController.base.js
+++ towerController.base.js
@@ -1,6 +1,5 @@
 function run(tower) {
     let hostiles = tower.room.find(FIND_HOSTILE_CREEPS);
-    console.log(hostiles ? 'no hostiles' : hostiles.length);
     if (hostiles.length > 0) {
         let username = hostiles[0].owner.username;
         Game.notify(`User ${username} spotted in room ${tower.room.name}`);
