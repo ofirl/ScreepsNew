6af06149196422b5ae0ea07b3c3469d0654a001c
Ofir Levi <ofirl24@gmail.com>
2020-04-25 00:34:28 +0300

    fix

--- creepRole.transport.js
+++ creepRole.transport.js
@@ -60,7 +60,7 @@ function run(creep) {
                 },
                 storage: {
                     selector: () =>
-                        creep.room.findClosestByRange(FIND_STRUCTURES, {
+                        creep.pos.findClosestByRange(FIND_STRUCTURES, {
                             filter: (s) => s.structureType === STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] < s.store.getCapacity(RESOURCE_ENERGY)
                         }),
                     // validator: (c) => c.store[RESOURCE_ENERGY] < c.store.getCapacity(RESOURCE_ENERGY),
